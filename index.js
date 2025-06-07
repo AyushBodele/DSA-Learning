32455fa20f9fe7837a271f07db117a5af3029e46
AyushBodele <ayushbodele83@gmail.com>
2025-06-08 00:00:53 +0530

    To find no of negative element in array

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let arr = [10, 20, 1, 47, 7];
-
-function searchElement(arr, element) {
+let arr = [10, 20, 1, 47, -7, -8];
+count = 0
+function searchElement(arr) {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === element) {
-      return i;
+    if (arr[i] < 0) {
+      count++;
     }
   }
-  return -1;
+  return count;
 }
-const result = searchElement(arr, 10); // returns 3
+const result = searchElement(arr); // returns 3
 console.log(result); // Output: 3
